a999a09934c23eae95af4c571223ea5f855d47b9
Macrichie <makanjuolakunle@gmail.com>
2017-08-01 18:56:10 +0100

    Added gitignore

---
+++ test/test_oop.js
@@ -0,0 +1,12 @@
+const assert = require('chai').assert;
+const animimalIndex = require('../index');
+
+
+describe("To create a class ", ()=> {
+
+    describe("should return 0 for an empty array", ()=> {
+        it("should return string", ()=> { 
+            assert.equal(aritGeo([]), 0);
+        });   
+    });
+});
